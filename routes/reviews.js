10beafced2f5cc7f1684363e9ad70a125ebab5b5
Husain007 <94444107+HusainBhattiwala@users.noreply.github.com>
2023-01-26 09:57:32 +0530

    Update reviews.js

--- routes/reviews.js
+++ routes/reviews.js
@@ -1,18 +1,14 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router({ mergeParams: true });
-const reviews = require("../controllers/reviews");
-const campground = require("../models/campground");
-const Review = require("../models/review");
-const catchAsync = require("../utils/catchAsync");
-const { isLoggedIn, isReviewAuthor, validateReview } = require("../middleware");
+const { validateReview, isLoggedIn, isReviewAuthor } = require('../middleware');
+const Campground = require('../models/campground');
+const Review = require('../models/review');
+const reviews = require('../controllers/reviews');
+const ExpressError = require('../utils/ExpressError');
+const catchAsync = require('../utils/catchAsync');
 
-router.post("/", isLoggedIn, validateReview, catchAsync(reviews.createReview));
+router.post('/', isLoggedIn, validateReview, catchAsync(reviews.createReview))
 
-router.delete(
-  "/:reviewId",
-  isLoggedIn,
-  isReviewAuthor,
-  catchAsync(reviews.deleteReview)
-);
+router.delete('/:reviewId', isLoggedIn, isReviewAuthor, catchAsync(reviews.deleteReview))
 
 module.exports = router;
